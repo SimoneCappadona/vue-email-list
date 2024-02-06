8e60403cc53d9e82fbced511d5de916b1e8dad71
SimoneCappadona <simocappa95@gmail.com>
2024-02-06 15:06:39 +0100

    add button on click print random mail (bonus)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const { createApp } = Vue
-
-  createApp({
-    data() {
-      for (let i = 0; i < 10; i++)
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=>{
-            console.log(email.data.response);
-        });
+createApp({
+  data(){
       return {
-        
+          randomMail: []    
       }
-    }
-      
+  },
+  created(){
+  },
+  methods:{
+      generate() {
+          this.randomMail = [];
+          this.count = 0
+          for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+              .then((email) => {
+                this.randomMail.push(email.data.response);
+                if (this.randomMail.length === 10) {
+                  this.count=10;
+                }
+              })
+          }
+      }
+  }     
   }).mount('#app')
 
 
